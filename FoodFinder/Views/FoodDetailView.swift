8785ae5e9b30d75c3eab4a7d67fa2add6f551e49
Alistair Fraser <afraser@lcs.on.ca>
2023-04-22 00:56:15 -0400

    Created project and completed initial organization of views.

---
+++ FoodFinder/Views/FoodDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  FoodDetailView.swift
+//  FoodFinder
+//
+//  Created by Alistair Fraser on 2023-04-22.
+//
+
+import SwiftUI
+
+struct FoodDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FoodDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FoodDetailView()
+    }
+}
